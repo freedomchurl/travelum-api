0b3f66e9334b778c27c070ade7286dd98b45e793
freedomchurl <freedomchurl@naver.com>
2022-03-24 04:08:24 +0900

    Init Project

---
+++ src/test/java/com/vaninside/travelum/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaninside.travelum.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
